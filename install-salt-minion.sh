87191ad8507afdeaa4ceffbd6c4d902dee781457
binups <binu@logrhythm.com>
2022-10-11 11:36:21 +0530

    script change for ubuntu 18.04

--- install-salt-minion.sh
+++ install-salt-minion.sh
@@ -11,11 +11,15 @@ fi
 ip_address=$1
 set -x
 sudo apt install python-software-properties
-wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+#wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 #sudo su
-sudo echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' >> /etc/apt/sources.list.d/saltstack.list
+sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3004/salt-archive-keyring.gpg
+
+#sudo echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' >> /etc/apt/sources.list.d/saltstack.list
+sudo echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3004 bionic main" | sudo tee /etc/apt/sources.list.d/salt.list
+
 #exit
-sudo apt update
+sudo apt-get update -y
 sudo apt install salt-minion
 sudo sed -i -e 's@\#master: salt@master: '$ip_address'@g' /etc/salt/minion
 
