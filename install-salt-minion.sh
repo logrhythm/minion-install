4b48c60be9c58135800f035a2034a352da7fa722
binups <binu@logrhythm.com>
2022-10-17 21:46:19 +0530

    new salt script

--- install-salt-minion.sh
+++ install-salt-minion.sh
@@ -10,7 +10,7 @@ fi
 
 ip_address=$1
 set -x
-sudo apt install python-software-properties
+#sudo apt install python-software-properties
 #wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 #sudo su
 sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3004/salt-archive-keyring.gpg
@@ -19,8 +19,8 @@ sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.sal
 sudo echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3004 bionic main" | sudo tee /etc/apt/sources.list.d/salt.list
 
 #exit
-sudo apt-get update -y
-sudo apt install salt-minion
+sudo apt-get -y  update
+sudo apt-get install -y  salt-minion
 sudo sed -i -e 's@\#master: salt@master: '$ip_address'@g' /etc/salt/minion
 
 sudo systemctl enable salt-minion.service
