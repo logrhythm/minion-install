422da6048592c2a53f487e3adc55a2184dd273bb
binupaul82 <80387853+binupaul82@users.noreply.github.com>
2023-01-02 13:50:58 +0530

    Update install-salt-minion.sh

--- install-salt-minion.sh
+++ install-salt-minion.sh
@@ -18,10 +18,10 @@ sudo apt-get install -y python3-software-properties
 
 #wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 #sudo su
-sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3004/salt-archive-keyring.gpg
+sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3005/salt-archive-keyring.gpg
 
 #sudo echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' >> /etc/apt/sources.list.d/saltstack.list
-sudo echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3004 bionic main" | sudo tee /etc/apt/sources.list.d/salt.list
+sudo echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3005 bionic main" | sudo tee /etc/apt/sources.list.d/salt.list
 
 #exit
 sudo apt-get -y  update
