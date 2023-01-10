87011f25ac18cf8624ddfca9f6008362544bdb4b
binups <binu@logrhythm.com>
2023-01-10 12:19:45 +0530

    script to install salt minion script in ubuntu 22

--- install-salt-minion.sh
+++ install-salt-minion.sh
@@ -16,12 +16,10 @@ sudo apt-get -y  update
 
 sudo apt-get install -y python3-software-properties
 
-#wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-#sudo su
-sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3005/salt-archive-keyring.gpg
-
-#sudo echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' >> /etc/apt/sources.list.d/saltstack.list
-sudo echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3005 bionic main" | sudo tee /etc/apt/sources.list.d/salt.list
+sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/3005/salt-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/3005 jammy main" | sudo tee /etc/apt/sources.list.d/salt.list
+#sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3005/salt-archive-keyring.gpg
+#sudo echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/18.04/amd64/3005 bionic main" | sudo tee /etc/apt/sources.list.d/salt.list
 mv /etc/salt /etc/salt.bak
 #exit
 sudo apt-get -y  update
