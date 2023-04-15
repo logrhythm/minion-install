1acde493abdc12f219680d0ed62afacfe93cb0f9
binupaul82 <80387853+binupaul82@users.noreply.github.com>
2023-04-15 20:28:47 +0530

    Update install-salt-minion.sh

--- install-salt-minion.sh
+++ install-salt-minion.sh
@@ -15,6 +15,7 @@ set -x
 sudo apt-get -y  update
 
 sudo apt-get install -y python3-software-properties
+sudo apt-get install curl -y
 
 sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/3005/salt-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/3005 focal main" | sudo tee /etc/apt/sources.list.d/salt.list
